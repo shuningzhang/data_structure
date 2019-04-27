3dc8b912ad2075e1f87698aa7c3034a2c6c16f3d
Sunny Zhang <shuningzhang@126.com>
2019-04-27 08:48:42 +0800

    algorithm: add shell sort
    Signed-off-by: Sunny Zhang <shuningzhang@126.com>

--- algorithm/00-sort/00-insert_sort/shell_sort.c
+++ algorithm/00-sort/00-insert_sort/shell_sort.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 
-/* 描述： 直接插入排序，该函数最终将数据排列成升序。 */
+void print_data(int *data, int nitems);
+
+void insert_sort(int *data, int nitems, int offset, int gap)
+{
+	int sorted_pos = 0;
+	int cur_pos = 0;
+	int count = nitems / gap;
+	int cur_data = 0;
+
+	for (sorted_pos = offset; sorted_pos < nitems - gap + offset; sorted_pos = sorted_pos + gap) {
+		if (sorted_pos + gap >= nitems) {
+			cur_data = data[nitems - 1];
+		} else {
+			cur_data = data[sorted_pos + gap];
+		}
+		for (cur_pos = sorted_pos; cur_pos >= offset; cur_pos -= gap ) {
+			if (cur_data < data[cur_pos]) {
+				data[cur_pos + gap] = data[cur_pos];	
+			} else {
+				break;
+			}
+		}
+		data[cur_pos + gap] = cur_data;
+	}
+}
+
+/* 描述： 希尔排序，该函数最终将数据排列成升序。 */
 void shell_sort(int *data, int nitems)
 {
+	int gap = nitems / 2;
+	int offset = 0;
+
+	while (gap >= 1) {
+		printf("\n");
+		for (offset = 0; offset < gap; ++ offset ) {
+			insert_sort(data, nitems, offset, gap);
+			print_data(data, nitems);
+		}
+		gap = gap /2;
+	}
 }
 
 void print_data(int *data, int nitems)
@@ -19,7 +56,7 @@ void print_data(int *data, int nitems)
 }
 int main(void)
 {
-	int data[] = {9, 7, 2, 3, 6, 8, 5, 4, 1};
+	int data[] = {9, 7, 5, 4, 6, 8, 2, 3, 1};
 	print_data(data, sizeof(data)/sizeof(int));
 	shell_sort(data, sizeof(data)/sizeof(int));
 	print_data(data, sizeof(data)/sizeof(int));
